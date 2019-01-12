260ffb16e09ad52564e59e5a638deb67f881f728
johnbonda <john@belfricsbt.com>
2019-01-12 13:14:15 +0530

    First commit

---
+++ model/company.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: "companys",
+    fields: [
+        {  
+            name: 'dappid',
+            type: 'String', 
+            length: 255,
+        },
+        {
+            name: 'company',
+            type: 'String',
+            length: 255,
+        },
+        {
+            name: 'country',
+            type: 'String',
+            length: 255,
+        },
+        {
+            name: 'name',
+            type: 'String',
+            length: 255,
+        }
+    ]
+}
